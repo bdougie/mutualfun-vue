c1f228a5649936a1613ec707e08d3597cee98e88
brian <ilikerobot@gmail.com>
2020-01-20 21:26:33 -0800

    adds twitch player

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import LoadScript from "vue-plugin-load-script";
 
-Vue.config.productionTip = false
+Vue.use(LoadScript);
+
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount("#app");
